4627b09a076568fab696be81fa74f8927625a8f1
Ameenacusat <busharatk0@gmail.com>
2026-08-31 23:12:29 +0530

    Connect frontend to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [answer, setAnswer] = useState("");
 
   async function search() {
-    const response = await fetch("http://127.0.0.1:8000/search", {
+    const response = await fetch("https://ai-notes-search-2.onrender.com/search", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
